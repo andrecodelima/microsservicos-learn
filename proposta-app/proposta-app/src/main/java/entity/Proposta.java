1256253bd5ed7fe456ad09973e8b3a5c08a4d512
Andre Luis Lima da Silva - Ilha Service Tecnologia <andre.lima@corp.int.gov.br>
2024-03-05 17:02:33 -0300

    new project

---
+++ proposta-app/proposta-app/src/main/java/entity/Proposta.java
@@ -0,0 +1,24 @@
+package entity;
+
+import jakarta.persistence.*;
+
+public class Proposta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double valorSolicitado;
+
+    private int prazoPagamento;
+
+    private Boolean aprovada;
+
+    private boolean integrada;
+
+    private String observacao;
+
+    @OneToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuário;
+}
